5426b75900bd6c11d0448b0f06700079f849ece2
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-09-17 14:52:22 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EggsInteractiveGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int chicken1;
+            int chicken2;
+            int chicken3;
+            int chicken4;
+            int chicken5;
+
+            chicken1 = Convert.ToInt32(textBox1.Text);
+            chicken2 = Convert.ToInt32(textBox2.Text);
+            chicken3 = Convert.ToInt32(textBox3.Text);
+            chicken4 = Convert.ToInt32(textBox4.Text);
+            chicken5 = Convert.ToInt32(textBox5.Text);
+            int eggs;
+            int eggsRemaining;
+            int dozen;
+            eggs = chicken1 + chicken2 + chicken3 + chicken4 + chicken5;
+            eggsRemaining = eggs % 12;
+            dozen = eggs / 12;
+            label2.Text = eggs + " is " + dozen + " dozen with " + eggsRemaining + " left over.";
+
+        }
+    }
+}
